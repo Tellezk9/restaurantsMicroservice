685fca0f751e055667650519e0f10ca7454c1a89
Tellezk9 <tellezk9@gmail.com>
2023-06-09 00:11:07 -0500

    change-dish-state

--- src/main/java/com/pragma/powerup/usermicroservice/domain/api/IRestaurantServicePort.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/api/IRestaurantServicePort.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface IRestaurantServicePort {
     void saveRestaurant(Restaurant restaurant);
     Restaurant getRestaurant(Integer idOwner, Integer idRestaurant);
-    List<Restaurant> getRestaurants();
+    List<Restaurant> getRestaurants(Integer page);
     List<Restaurant> getOwnerRestaurants(Integer idOwner);
     void saveRestaurantEmployee(Employee employee);
 }
